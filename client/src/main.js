bf8443f42b7066d9819a126f7aa3c3f256a85cf2
Daniel Sundholm <dajsun@utu.fi>
2020-03-22 16:58:02 +0200

    Added initial store, retrieves via backend scraper

--- client/src/main.js
+++ client/src/main.js
@@ -15,5 +15,8 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  created () {
+    this.$store.dispatch('getSports')
+  },
   render: h => h(App)
 }).$mount('#app')
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import services from '../services'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    sports: []
   },
   mutations: {
+    retrieveSports: async (state) => {
+      const sportsData = await services.getSports()
+      console.log("Sportsdata", sportsData)
+      state.sports.push(sportsData)
+    }
   },
   actions: {
+    getSports: ({ commit }) => {
+      commit('retrieveSports')
+    }
   },
   modules: {
   }
