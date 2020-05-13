aad232b73d81365626d5ae24fa92e5d618def390
Daniel Sundholm <danieljoonatan.sundholm@gmail.com>
2020-05-13 18:38:38 +0300

    More responsive sports class coloring

--- client/src/components/SportsClass.vue
+++ client/src/components/SportsClass.vue
@@ -1,5 +1,10 @@
 <template>
-  <b-col :class="[sportsBase, emptyName ? '' : [activeClass, cardColor]]">{{ name }}</b-col>
+  <b-col
+    @mouseover="hover = true"
+    @mouseleave="hover = false"
+    :class="[sportsBase, emptyName ? '' : [activeClass, cardColor]]">
+      {{ name }}
+  </b-col>
 </template>
 
 <script>
@@ -27,28 +32,31 @@ export default {
       if (this.sport.length < 3) return ''
       switch (this.sport[3]) {
         case "t1":
-          color = "secondary"
+          color = "sportsGray"
           break
         case "t2":
-          color = "success"
+          color = "sportsGreen"
           break
         case "t3":
-          color = "danger"
+          color = "sportsRed"
           break
         case "t4":
-          color = "danger"
+          color = "sportsOrange"
           break
         case "t5":
-          color = "info"
+          color = "sportsBlue"
           break
         case "t6":
-          color = "warning"
+          color = "sportsYellow"
           break
         case "t7":
-          color = "primary"
+          color = "sportsDarkGreen"
+          break
+        case "t9":
+          color = "sportsDarkRed"
           break
         }
-      return `border rounded border-${color}`
+      return color
     }
   }
 }
@@ -63,5 +71,32 @@ export default {
 
 .sportsActive {
   cursor: pointer;
+  color: rgba(49, 64, 78, 1);
+  background-color: rgba(var(--rgb), 0);
+  border: solid 1px;
+  border-radius: 5px;
+  border-color: rgba(var(--rgb), 1);
+  transition: background-color 0.5s, color 0.3s;
 }
+
+.sportsActive:hover {
+  background-color: rgba(var(--rgb), 1);
+  color: white;
+}
+
+.sportsGray { --rgb: 163, 163, 163; }
+
+.sportsGreen { --rgb: 0, 175, 102; }
+
+.sportsDarkGreen { --rgb: 0, 120, 41; }
+
+.sportsYellow { --rgb: 255, 163, 0; }
+
+.sportsBlue { --rgb: 0, 169, 224; }
+
+.sportsRed { --rgb: 228, 78, 52; }
+
+.sportsDarkRed { --rgb: 204, 24, 11; }
+
+.sportsOrange { --rgb: 255, 104, 0; }
 </style>
