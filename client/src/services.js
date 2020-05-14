03506f1ec024d3d04ea2d944a5d2089e4b2f8c0e
Daniel Sundholm <danieljoonatan.sundholm@gmail.com>
2020-05-14 17:49:56 +0300

    Updated build

--- client/src/services.js
+++ client/src/services.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
-
 import axios from 'axios'
+
+require('dotenv').config()
 
 const getSports = async () => {
     const response = await axios.get(`${process.env.VUE_APP_API_ENDPOINT}/sports`)
