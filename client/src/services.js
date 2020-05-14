f2e7d169286f8250d9a3369afb7c824931c66c8e
Daniel Sundholm <danieljoonatan.sundholm@gmail.com>
2020-05-14 16:37:14 +0300

    Request url changes for production

--- client/src/services.js
+++ client/src/services.js
@@ -1,12 +1,18 @@
 import axios from 'axios'
+let baseUrl = 'http://localhost:3003'
+
+if (process.env.NODE_ENV === 'production') {
+    baseUrl = 'https://sport-cal.herokuapp.com'
+}
 
 const getSports = async () => {
-    const response = await axios.get('http://localhost:3003/api/sports')
+    console.log("Baseurl:", baseUrl)
+    const response = await axios.get(`${baseUrl}/api/sports`)
     return response.data
 }
 
 const getGyms = async () => {
-    const response = await axios.get('http://localhost:3003/api/gyms')
+    const response = await axios.get(`${baseUrl}/api/gyms`)
     return response.data
 }
 
