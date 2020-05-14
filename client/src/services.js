9a7c2400ca32071bd72ebaf58a6280688e747310
Daniel Sundholm <danieljoonatan.sundholm@gmail.com>
2020-05-14 18:21:57 +0300

    Updated API url and new build

--- client/src/services.js
+++ client/src/services.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 require('dotenv').config()
 
 const getSports = async () => {
-    const response = await axios.get(`${process.env.VUE_APP_API_ENDPOINT}sports`)
+    const response = await axios.get(`${process.env.VUE_APP_API_ENDPOINT}api/sports`)
     return response.data
 }
 
 const getGyms = async () => {
-    const response = await axios.get(`${process.env.VUE_APP_API_ENDPOINT}gyms`)
+    const response = await axios.get(`${process.env.VUE_APP_API_ENDPOINT}api/gyms`)
     return response.data
 }
 
