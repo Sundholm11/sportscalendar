d7c0783bf8cfbdd6c681ac8bb1a474bf5d5e701e
Daniel Sundholm <danieljoonatan.sundholm@gmail.com>
2023-02-08 20:22:23 +0200

    Api tweaking

--- client/src/services.js
+++ client/src/services.js
@@ -1,14 +1,12 @@
 import axios from 'axios'
 
-require('dotenv').config()
-
 const getSports = async () => {
-	const response = await axios.get(`${process.env.VUE_APP_API_ENDPOINT}api/sports`)
+	const response = await axios.get(`api/sports`)
 	return response.data
 }
 
 const getGyms = async () => {
-	const response = await axios.get(`${process.env.VUE_APP_API_ENDPOINT}api/gyms`)
+	const response = await axios.get(`api/gyms`)
 	return response.data
 }
 
